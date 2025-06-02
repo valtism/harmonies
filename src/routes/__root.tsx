633472b3b30637a78de99df81c804e7fb5795a7d
Dan Wood <daniel4wood@icloud.com>
2025-06-02 21:33:55 +1000

    Add link to home for quick restarts

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,13 @@
-import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <h1>Harmonies</h1>
-    
+      <Link to="/" className="cursor-default">
+        <h1 className="text-2xl font-medium">Harmonies</h1>
+      </Link>
+
       <Outlet />
       <TanStackRouterDevtools />
     </>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,14 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
 import { useState } from "react";
 
-// 6 character hex code
-const randomRoomId = Math.floor(Math.random() * 16777215).toString(16);
-
 export const Route = createFileRoute("/")({
   component: Index,
 });
 
 function Index() {
+  // 6 character hex code
+  const randomRoomId = Math.floor(Math.random() * 16777215).toString(16);
+
   const [name, setName] = useState(randomRoomId);
 
   return (
@@ -17,9 +17,9 @@ function Index() {
         type="text"
         value={name}
         onChange={(e) => setName(e.currentTarget.value)}
-        className="bg-stone-600 rounded"
+        className="rounded bg-stone-600"
       />
-      <Link to="/$roomId" params={{ roomId: randomRoomId }}>
+      <Link to="/$roomId" params={{ roomId: name }}>
         Create a game
       </Link>
     </div>
